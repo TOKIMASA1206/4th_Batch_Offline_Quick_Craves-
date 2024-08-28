a2737a3586b5e4d857d223253c0eb61aa0d5b0fd
坂下輝 <>
2024-08-28 09:51:31 +0800

    commit changes on admin-dashboard branch

--- resources/views/admin/menus/modal/status.blade.php
+++ resources/views/admin/menus/modal/status.blade.php
@@ -1,7 +1,8 @@
 {{-- Add --}}
-<div class="modal fade" id="add-category-{{-- $category->id --}}">
+<div class="modal fade palanquin-dark-regular" id="add-category-{{-- $category->id --}}">
     <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content bg-white" style="width: 700px !important">
+        <div class="modal-content" style="background-color: rgb(199, 199, 245) !important ">
+            {{-- サイズ変えたい --}}
             <div class="row justify-content-center">
                 <div class="col-8">
                     <form action="{{-- route('profile.update') --}}" method="POST" enctype="multipart/form-data">
@@ -10,37 +11,72 @@
                         <h2 class="h3 mb-3 amarante-regular text-center">Add Menu</h2>
 
                         <div class="row mb-3">
-                            <div class="col-6">
+                            <div class="col-12">
                                 <input type="file" name="avatar" id="avatar" class="form-control form-control-sm mt-1" aria-describedby="avatar-info">
                                 <div id="avatar-info" class="form-text">
                                     Acceptable format: jpeg, jpg, png, gif only <br>
                                     Max file size is 1048kB
                                 </div>
                             </div>
-                            <div class="col-6">
-                            </div>
                         </div>
+
                         <div class="mb-3">
-                            <label for="name" class="form-label fw-bold">Name</label>
-                            <input type="text" name="name" id="name" class="form-control" autofocus>
+                            <input type="text" name="name" id="name" class="form-control" placeholder="name" autofocus>
                             {{-- @error('name')
                                 <div class="text-danger small">{{ $message }}</div>
                             @enderror --}}
                         </div>
+
                         <div class="mb-3">
-                            <label for="email" class="form-label fw-bold">E-Mail Address</label>
-                            <input type="email" name="email" id="email" class="form-control">
-                            {{-- @error('email')
+                            <input type="number" name="price" id="price" class="form-control" placeholder="price" autofocus>
+                            {{-- @error('name')
                                 <div class="text-danger small">{{ $message }}</div>
                             @enderror --}}
                         </div>
+
                         <div class="mb-3">
                             <label for="introduction" class="form-label fw-bold">Introduction</label>
-                           <textarea name="introduction" id="introduction" rows="5" class="form-control" placeholder="Describe yourself"></textarea>
-
+                           <textarea name="introduction" id="introduction" rows="5" class="form-control" placeholder="Description"></textarea>
                         </div>
 
-                        <button type="submit" class="btn btn-warning px-5">Save</button>
+                        <div class="mb-3">
+                            <select class="form-select" aria-label="Default select example">
+                                <option selected>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                              </select>
+                        </div>
+
+                        <div class="mb-3">
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" role="switch" id="size">
+                                <label class="form-check-label mt-2 ms-2" for="size">Custom Menu( +size)</label>
+                            </div>
+                        </div>
+
+                        <div class="mb-3">
+                            <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" role="switch" id="option">
+                                <label class="form-check-label  mt-2 ms-2" for="option">Custom Menu( +option)</label>
+                            </div>
+                        </div>
+
+                        <div class="modal-footer border-0">
+                            <form action="{{-- route('admin.posts.deactivate', $post->id) --}}" method="POST">
+                                @csrf
+
+
+                                <button type="button" class="btn" data-bs-dismiss="modal" style="border-color:rgb(81, 51, 179)">
+                                    <span class="text-dark">Cancel</span>
+                                </button>
+
+                                <button type="submit" class="btn" style="background-color: rgb(81, 51, 179)">
+                                    <span class="text-white">+ Add</span>
+                                </button>
+                            </form>
+                        </div>
+
                     </form>
                 </div>
             </div>
@@ -50,111 +86,148 @@
 
 
 {{-- Edit --}}
-<div class="modal fade" id="edit-category-{{-- $category->id --}}">
-    <div class="modal-dialog">
-        <div class="modal-content border-warning">
-            <div class="modal-header border-warning">
-                <h3 class="h5 modal-title text-warning">
-                    <i class="fa-solid fa-pen"></i> Edit Menu
-                </h3>
-            </div>
-            <form action="{{-- route('admin.categories.update',$category->id) --}}" method="POST" >
-                @csrf
-                @method('PATCH')
-                <div class="d-flex justify-content-center modal-body">
-                    <div class="input-group mb-3 mt-4 w-75">
-                        <input type="text" name="category" class="form-control" value="{{-- $category->name --}}" aria-label="Recipient's username" aria-describedby="button-addon2">
-                        <button class="btn btn-outline-warning" type="submit" id="button-addon2">Edit</button>
+    <div class="modal fade palanquin-dark-regular" id="edit-menu-{{-- $category->id --}}">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content" style="background-color: rgb(199, 199, 245) !important ">
+                {{-- サイズ変えたい --}}
+                <div class="row justify-content-center">
+                    <div class="col-8">
+                        <form action="{{-- route('profile.update') --}}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            @method('PATCH')
+                            <h2 class="h3 mb-3 amarante-regular text-center">Edit Menu</h2>
+
+                            <div class="row mb-3">
+                                <div class="col-12">
+                                    <input type="file" name="avatar" id="avatar" class="form-control form-control-sm mt-1" aria-describedby="avatar-info">
+                                    <div id="avatar-info" class="form-text">
+                                        Acceptable format: jpeg, jpg, png, gif only <br>
+                                        Max file size is 1048kB
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <input type="text" name="name" id="name" class="form-control" placeholder="name" autofocus>
+                                {{-- @error('name')
+                                    <div class="text-danger small">{{ $message }}</div>
+                                @enderror --}}
+                            </div>
+
+                            <div class="mb-3">
+                                <input type="number" name="price" id="price" class="form-control" placeholder="price" autofocus>
+                                {{-- @error('name')
+                                    <div class="text-danger small">{{ $message }}</div>
+                                @enderror --}}
+                            </div>
+
+                            <div class="mb-3">
+                                <label for="introduction" class="form-label fw-bold">Introduction</label>
+                               <textarea name="introduction" id="introduction" rows="5" class="form-control" placeholder="Description"></textarea>
+                            </div>
+
+                            <div class="mb-3">
+                                <select class="form-select" aria-label="Default select example">
+                                    <option selected>Open this select menu</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                  </select>
+                            </div>
+
+                            <div class="mb-3">
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" role="switch" id="size">
+                                    <label class="form-check-label mt-2 ms-2" for="size">Custom Menu( +size)</label>
+                                </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" role="switch" id="option">
+                                    <label class="form-check-label  mt-2 ms-2" for="option">Custom Menu( +option)</label>
+                                </div>
+                            </div>
+
+                            <div class="modal-footer border-0">
+                                <form action="{{-- route('admin.posts.deactivate', $post->id) --}}" method="POST">
+                                    @csrf
+
+
+                                    <button type="button" class="btn" data-bs-dismiss="modal" style="border-color:rgb(81, 51, 179)">
+                                        <span class="text-dark">Cancel</span>
+                                    </button>
+
+                                    <button type="submit" class="btn" style="background-color: rgb(81, 51, 179)">
+                                        <span class="text-white">+ Add</span>
+                                    </button>
+                                </form>
+                            </div>
+
+                        </form>
                     </div>
                 </div>
-            </form>
+            </div>
         </div>
     </div>
-</div>
 
 {{-- Delete --}}
-<div class="modal fade" id="delete-category-{{-- $category->id --}}">
-    <div class="modal-dialog">
-        <div class="modal-content border-danger">
-            <div class="modal-header border-danger">
-                <h3 class="h5 modal-title text-danger">
-                    <i class="fa-solid fa-trash"></i> Delete Category
-                </h3>
+    <div class="modal fade palanquin-dark-regular" tabindex="-1" id="delete-menu-{{-- $category->id --}}">
+        <div class="modal-dialog">
+          <div class="modal-content" style="background-color: rgb(199, 199, 245) !important ">
+            <div class="modal-header">
+              <h5 class="modal-title" style="color: rgb(81, 51, 179)">Delete Menu</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <form action="{{-- route('admin.categories.destroy',$category->id) --}}" method="POST" >
-                @csrf
-                @method('DELETE')
-
-                       <h4 class="text-center mt-4">Are you sure delete "{{-- $category->name --}}"?</h4>
-
-                <div class="text-center mt-3 mb-3">
-                    <button class="btn btn-outline-danger rounded form-control w-50" type="submit">Delete</button>
-                </div>
-            </form>
+            <div class="modal-body">
+              <p class="text-dark">Are you sure you want to <span class="text-danger">Delete</span> " ######## " ?</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn" data-bs-dismiss="modal" style="border-color:rgb(81, 51, 179)"><span style="color: rgb(81, 51, 179)">Close</span></button>
+              <button type="button" class="btn text-white" style="background-color: rgb(81, 51, 179)">Delete</button>
+            </div>
+          </div>
         </div>
     </div>
-</div>
 
 
 {{-- Deactivate --}}
-<div class="modal fade" id="deactivate-post-{{-- $post->id --}}">
-    <div class="modal-dialog">
-        <div class="modal-content border-secondary">
-            <div class="modal-header border-secondary">
-                <h3 class="h5 modal-title text-secondary">
-                    <i class="fa-solid fa-eye-slash"></i> Hide Post
-                </h3>
-            </div>
 
+    <div class="modal fade palanquin-dark-regular" tabindex="-1" id="inactivate-post-{{-- $post->id --}}">
+        <div class="modal-dialog">
+          <div class="modal-content" style="background-color: rgb(199, 199, 245) !important ">
+            <div class="modal-header">
+              <h5 class="modal-title" style="color: rgb(81, 51, 179)">Inactivate Menu</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
             <div class="modal-body">
-                <img src="{{-- $post->image --}}" alt="{{-- $post->description --}}" class="image-lg me-2">
-                Are you sure you want to hide?
+              <p class="text-dark">Are you sure you want to <span class="text-danger">Inactivate</span> " ######## " ?</p>
             </div>
-            <p class="ms-3">{{-- $post->description --}}</p>
-
-
-            <div class="modal-footer border-0">
-                <form action="{{-- route('admin.posts.deactivate', $post->id) --}}" method="POST">
-                    @csrf
-                    @method('DELETE')
-
-                    <button type="button" class="btn btn-outline-secondary btn-sm" data-bs-dismiss="modal">Cancel</button>
-
-                    <button type="submit" class="btn btn-secondary btn-sm">Hide</button>
-                </form>
+            <div class="modal-footer">
+              <button type="button" class="btn" data-bs-dismiss="modal" style="border-color:rgb(81, 51, 179)"><span style="color: rgb(81, 51, 179)">Close</span></button>
+              <button type="button" class="btn text-white" style="background-color: rgb(81, 51, 179)">Inactivate</button>
             </div>
+          </div>
         </div>
     </div>
-</div>
 
 
-{{--  POST  --}}
+{{--  Activate  --}}
 
-<div class="modal fade" id="activate-post-{{-- $post->id --}}">
+<div class="modal fade palanquin-dark-regular" tabindex="-1" id="activate-post-{{-- $post->id --}}">
     <div class="modal-dialog">
-        <div class="modal-content border-primary">
-            <div class="modal-header border-primary">
-                <h3 class="h5 modal-title text-primary">
-                    <i class="fa-solid fa-eye text-primary"></i> Visible Menu
-                </h3>
-            </div>
-
-            <div class="modal-body">
-                <img src="{{-- $post->image --}}" alt="{{-- $post->description --}}" class="image-lg me-2">
-                Are you sure you want to Activate <span class="fw-bold"></span>?
-            </div>
-            <p class="ms-3">{{-- $post->description --}}</p>
-
-            <div class="modal-footer border-0">
-                <form action="{{-- route('admin.posts.activate',$post->id) --}}" method="POST">
-                    @csrf
-                    @method('PATCH')
-
-                    <button type="button" class="btn btn-outline-primary btn-sm" data-bs-dismiss="modal">Cancel</button>
-
-                    <button type="submit" class="btn btn-primary btn-sm">Visible</button>
-                </form>
-            </div>
+      <div class="modal-content" style="background-color: rgb(199, 199, 245) !important ">
+        <div class="modal-header">
+          <h5 class="modal-title" style="color: rgb(81, 51, 179)">Activate Menu</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
+        <div class="modal-body">
+          <p class="text-dark">Are you sure you want to <span class="text-success">Activate</span> " ######## " ?</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn" data-bs-dismiss="modal" style="border-color:rgb(81, 51, 179)"><span style="color: rgb(81, 51, 179)">Close</span></button>
+          <button type="button" class="btn text-white" style="background-color: rgb(81, 51, 179)">Activate</button>
+        </div>
+      </div>
     </div>
 </div>
