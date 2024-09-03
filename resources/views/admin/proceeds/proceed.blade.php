7221fb1ad9cde0a38465870ab3d20c0bc8cf3553
坂下輝 <>
2024-09-03 09:59:02 +0800

    separeate files

---
+++ resources/views/admin/proceeds/proceed.blade.php
@@ -0,0 +1,5 @@
+<div class="tab-pane fade" id="v-pills-proceed" role="tabpanel" aria-labelledby="v-pills-proceed-tab">
+    <div class="fp_dashboard_body">
+        <h3>PROCEED</h3>
+    </div>
+</div>
