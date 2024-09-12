4252fe66911923af3fb4513110dbea3681d51e92
WATARU-OHKURA <wataruokura17@gmail.com>
2024-09-12 09:31:48 +0800

    create datatables

---
+++ database/migrations/2024_09_11_032423_create_profiles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('phone')->nullable();
+            $table->string('avatar')->default('/uploads/avatar.jpg');
+            $table->enum('gender', ['male', 'female', 'other'])->nullable();
+            $table->integer('age')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
--- database/migrations/2024_09_11_135204_create_voucher_user_table.php
+++ database/migrations/2024_09_11_135204_create_voucher_user_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('profiles', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+        Schema::create('voucher_user', function (Blueprint $table) {
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('voucher_id')->constrained('vouchers')->onDelete('cascade');
         });
     }
 
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('profiles');
+        Schema::dropIfExists('user_vouchers');
     }
 };
